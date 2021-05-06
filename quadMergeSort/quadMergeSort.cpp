dd322b498bc1b22b11da69a55398d5000dfd2222
Claudio Javier Rain Levicán <rainclaudio25@gmail.com>
2021-05-06 02:36:34 -0400

    avance6

--- quadMergeSort/quadMergeSort.cpp
+++ quadMergeSort/quadMergeSort.cpp
@@ -36,49 +36,107 @@ v merge_util(v avector, v bvector, int sizea, int sizeb){
   return dvector;
 }
 
-void Merge(float *A, int lb, int mid1,int mid2,int ub){
-  v Av,Bv,Cv;
- int n1 = mid1 - lb +1; // size of Av
- int n2 =  mid2 - mid1; // size of Bv
- int n3 =  ub - mid2; // size of Cv
+void Merge(float *A, int lb, int midleft,int midCentral,int midright,int ub){
+  v Av,Bv,Cv,Dv;
+  // MODIFICAR
+ int n1 = midleft - lb +1; // size of Av
+ int n2 =  midCentral - midleft; // size of Bv
+ int n3 =  midright - midCentral; // size of Cv
+ int n4 = ub - midright;
   for(int i = 0; i <  n1;++i){
    Av.push_back(A[lb + i]);
   }
   for(int j = 0; j  < n2; ++j){
-    Bv.push_back(A[mid1 + 1 + j]);
+    Bv.push_back(A[midleft + 1 + j]);
   }
   for(int k = 0; k < n3; ++k){
-    Cv.push_back(A[mid2 + 1 + k]);
+    Cv.push_back(A[midCentral + 1 + k]);
+  }
+  for(int k = 0; k < n4; ++k){
+    Dv.push_back(A[midright + 1 + k]);
   }
 
-  v vecvar = merge_util(Av,Bv,n1,n2);
-  v answer = merge_util(Cv,vecvar,n3,n1+n2);
+  v vecvar1 = merge_util(Av,Bv,n1,n2);
+  v vecvar2 = merge_util(Cv,Dv,n3,n4);
+  v answer = merge_util(vecvar1,vecvar2,n1+n2,n3+n4);
+
   for(int i = 0; i < answer.size(); ++i){
     A[i+lb] = answer[i];
   }
 
 }
-
-void quad_MergeSort(float *A,int lb, int ub){
-	if( ub - lb > 1){
-	int mid1 = lb + ((ub - lb) / 3);
-	int mid2 = lb + 2 * ((ub - lb) / 3) + 1;
-
-		quad_MergeSort(A,lb,mid1);
-		quad_MergeSort(A,mid1 + 1, mid2);
-		quad_MergeSort(A,mid2 + 1,ub);
-
-		Merge(A, lb,mid1,mid2, ub);
-
-	} else if (ub - lb == 1){
-    float n1 = A[lb];
-    float n2 = A[ub];
-    if(n2 <= n1){
-      A[lb] = n2;
-      A[ub] = n1;
+void compare(float *A, float n1, float n2, float n3,int lb){
+  if(n1 < n2){
+    if(n1 < n3){
+      if(n2 < n3){
+        A[lb] = n1;
+        A[lb + 1] = n2;
+        A[lb + 2] = n3;
+      } else {
+        A[lb] = n1;
+        A[lb + 1] = n3;
+        A[lb + 2] = n2;
+      }
+    } else {
+      A[lb] = n3;
+      A[lb + 1] = n1;
+      A[lb + 2] = n2;
     }
-	}
+  } else {
+    if(n2 < n3){
+      if(n1 < n3){
+        A[lb] = n2;
+        A[lb + 1] = n1;
+        A[lb + 2] = n3;
+      } else{
+        A[lb] = n2;
+        A[lb + 1] = n3;
+        A[lb + 2] = n1;
+      }
+    } else {
+      A[lb] = n3;
+      A[lb + 1] = n2;
+      A[lb + 2] = n1;
+    }
+  }
 }
+void util_sort(float *A, int lb, int ub){
+  float n1 = A[lb];
+  float n2 = A[lb + 1];
+  float n3 = A[ub];
+
+  switch (ub-lb) {
+    case 1:
+      if(n3 < n1){
+        A[lb] = n3;
+        A[ub] = n1;
+      }
+      break;
+    case 2:
+      compare(A,n1,n2,n3,lb);
+      break;
+
+    default: break;
+  }
+}
+void quad_MergeSort(float *A,int lb, int ub){
+	if( ub - lb > 2){
+
+	int midCentral = (ub + lb)/2;
+	int midleft =  (lb + midCentral)/2;
+  int midright = (midCentral + ub)/2;
+
+		quad_MergeSort(A,lb,midleft);
+		quad_MergeSort(A,midleft + 1, midCentral);
+		quad_MergeSort(A,midCentral + 1,midright);
+    quad_MergeSort(A,midright + 1,ub);
+		Merge(A, lb,midleft,midCentral,midright, ub);
+
+	} else {
+      util_sort(A,lb,ub);
+    }
+}
+
 
 double expermient(float *A,int tam){
   int nCases = 20;
